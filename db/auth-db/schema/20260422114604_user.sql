6b91cf8a9bfc811e9889d54c5eb83224a7ea8d37
MarkTerrious <named1006@gmail.com>
2026-04-23 18:21:19 +0900

    create db connection

---
+++ db/auth-db/schema/20260422114604_user.sql
@@ -0,0 +1,22 @@
+-- +goose Up
+CREATE TABLE users(
+  id         BIGSERIAL  PRIMARY KEY ,
+  email      TEXT       UNIQUE NOT NULL,
+  password   TEXT       NOT NULL,
+  first_name TEXT       NOT NULL,
+  last_name  TEXT       NOT NULL,
+  phone      TEXT,
+
+  is_active  BOOLEAN    NOT NULL DEFAULT TRUE,
+  role       USER_ROLE  NOT NULL DEFAULT 'customer',
+  
+  created_at TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
+  deleted_at TIMESTAMPTZ  ,      
+);
+
+CREATE INDEX idx_users_deleted_at ON users (deleted_at);
+
+
+-- +goose Down
+DROP TABLE IF EXISTS users;
