dc1cce9726f6a53b7cf061254dae4a3946aaf1a3
MarkTerrious <named1006@gmail.com>
2026-04-24 17:47:48 +0900

    set graphql and decimal

--- db/auth-db/schema/20260423093322_user.sql
+++ db/auth-db/schema/20260423093322_user.sql
@@ -1,5 +1,5 @@
 -- +goose Up
-CREATE TABLE users(
+CREATE TABLE IF NOT EXISTS users(
   id         BIGSERIAL  PRIMARY KEY ,
   email      TEXT       UNIQUE NOT NULL,
   password   TEXT       NOT NULL,
@@ -12,11 +12,12 @@ CREATE TABLE users(
   
   created_at TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
-  deleted_at TIMESTAMPTZ  ,      
+  deleted_at TIMESTAMPTZ
 );
 
-CREATE INDEX idx_users_deleted_at ON users (deleted_at);
+CREATE INDEX IF NOT EXISTS idx_users_deleted_at ON users (deleted_at);
+SELECT set_updated_at('users');
 
 
 -- +goose Down
-DROP TABLE IF EXISTS users;
+-- DROP TABLE IF EXISTS users;
