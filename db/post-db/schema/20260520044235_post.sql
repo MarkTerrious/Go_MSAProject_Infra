7a3ed609ab058410bde0f3da5ffbbb7edf167b09
MarkTerrious <named1006@gmail.com>
2026-06-04 12:50:13 +0900

    title: CreatePost body:   HTTP 관련 코드 Common 으로 이동   Image Provider 추가   (Gql, sqlc) Custom 값 추가   DB 스키마 수정 ( BigInt Auto Increment )

--- db/post-db/schema/20260520044235_post.sql
+++ db/post-db/schema/20260520044235_post.sql
@@ -1,8 +1,9 @@
 -- +goose Up
 CREATE TABLE IF NOT EXISTS posts(
-  id          BIGINT       PRIMARY KEY,
+  id          BIGINT       GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   user_id     BIGINT       NOT NULL,
   title       text         NOT NULL,
+  comment_num INT          NOT NULL DEFAULT 0,
   created_at  TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
   updated_at  TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
   deleted_at  TIMESTAMPTZ
@@ -14,9 +15,12 @@ CREATE INDEX IF NOT EXISTS idx_post_deleted_at ON posts(deleted_at);
 
 SELECT set_updated_at('posts');
 
+CREATE TYPE FORMAT_TYPE AS ENUM('text', 'image', 'url');
 CREATE TABLE IF NOT EXISTS post_contents(
-  id       BIGINT      PRIMARY KEY,
+  id       BIGINT      GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   post_id  BIGINT      NOT NULL,
+  image_id BIGINT,
+  format   FORMAT_TYPE NOT NULL,
   content  TEXT        NOT NULL,
   order_no SMALLINT    NOT NULL,
 
@@ -28,27 +32,28 @@ CREATE TABLE IF NOT EXISTS post_contents(
   CONSTRAINT fk_post_contents_unique
     UNIQUE (post_id, order_no)
 );
+CREATE INDEX IF NOT EXISTS idx_post_contents_multi_post ON post_contents(post_id, format, order_no);
 CREATE INDEX IF NOT EXISTS idx_post_contents_post ON post_contents(post_id, order_no);
 
-CREATE TYPE FORMAT_TYPE AS ENUM('image', 'video');
-CREATE TABLE IF NOT EXISTS post_media(
-  id        BIGINT      PRIMARY KEY,
-  post_id   BIGINT      NOT NULL,
-  format    FORMAT_TYPE NOT NULL,
-  furl      TEXT        NOT NULL,
-  order_no  SMALLINT    NOT NULL,
+CREATE TYPE IMAGE_STATUS AS ENUM('pending', 'completed', 'cancel');
+CREATE TABLE IF NOT EXISTS images(
+  id          BIGINT        GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id     BIGINT        NOT NULL,
+  category    TEXT          NOT NULL,
+  istatus     IMAGE_STATUS  NOT NULL,
+  origin_name TEXT          NOT NULL,
+  stored_name TEXT          NOT NULL,
+  created_at  TIMESTAMPTZ   NOT NULL DEFAULT NOW(),
 
-  CONSTRAINT fk_post_media_post_id
-    FOREIGN KEY (post_id)
-    REFERENCES posts(id)
-    ON DELETE CASCADE,
-
-  CONSTRAINT fk_post_media_unique
-    UNIQUE (post_id, order_no)
+  CONSTRAINT images_stored_name_unique
+    UNIQUE (stored_name)
 );
 
-CREATE INDEX IF NOT EXISTS idx_pmd_post ON post_media(post_id, order_no);
-
+-- Upload Index
+CREATE INDEX IF NOT EXISTS idx_img_user ON images(id, user_id, istatus);
+-- Select Index
+CREATE INDEX IF NOT EXISTS idx_img_istatus ON images(istatus);
 
 -- +goose Down
-SELECT 'down SQL query';
+DROP TYPE FORMAT_TYPE;
+DROP TYPE IMAGE_STATUS;
